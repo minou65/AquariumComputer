db2a99986a4816b9af4e561eea3e3488aad9fa4e
Andy <azogg65@bluewin.ch>
2026-01-18 08:01:19 +0100

    fix: save and restore time before reboot in NTP client

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 3
+#define VERSION_PATCH 4
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 141
+#define VERSION_BUILD 148
 #define VERSION_DATE "2026-01-18"
-#define VERSION_TIME "06:49:55"
-#define VERSION "1.0.3.141"
-#define VERSION_STR "1.0.3.141 (2026-01-18 06:49:55)"
+#define VERSION_TIME "07:54:33"
+#define VERSION "1.0.4.148"
+#define VERSION_STR "1.0.4.148 (2026-01-18 07:54:33)"
